01303153abd9bb5c951764ae3fa23eddd376551a
tsubasa1022 <tsubasa1022@gmail.com>
2019-07-08 23:29:52 +0900

    Update route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'messages/index'
-
+root to: 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
