e387bdc2c3b9894d93e1f7caecb8012a7462190d
tsubasa1022 <tsubasa1022@gmail.com>
2019-07-15 14:51:20 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-
-  get 'users/update'
-
   devise_for :users
-  root to: 'messages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'messages#index'
+  resources :users, only: [:edit, :update]
 end
