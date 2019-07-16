c4afd934b2c0c70fb51bf9754b60c38f49251c91
tsubasa1022 <tsubasa1022@gmail.com>
2019-07-16 22:02:51 +0900

    Create users table

--- db/migrate/20190716121238_create_groups.rb
+++ db/migrate/20190716121238_create_groups.rb
@@ -1,8 +1,9 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
-  def change
+  def change 
     create_table :groups do |t|
-
-      t.timestamps
+    t.string :name, null: false
+    t.index :name, unique: true
+    t.timestamps
     end
   end
 end
