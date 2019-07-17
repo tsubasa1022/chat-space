34a84bbc53b89d099603e6d30f7114167096889f
tsubasa1022 <tsubasa1022@gmail.com>
2019-07-17 21:05:54 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :groups
+  has_many :group_users
   has_many :groups, through: :group_users
 end
