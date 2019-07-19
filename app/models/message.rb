007cc52dbbc26e4bada03820b00fa83f128f7827
tsubasa1022 <tsubasa1022@gmail.com>
2019-07-19 14:18:15 +0900

    アソシエーション追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,8 @@
 class Message < ApplicationRecord
+  class Message < ApplicationRecord
+    belongs_to :group
+    belongs_to :user
+  
+    validates :content, presence: true, unless: :image?
+  end
 end
