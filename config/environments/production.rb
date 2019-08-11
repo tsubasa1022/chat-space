c51d34072c255722e6aeba6c4f99da9f0a2e7a9b
tsubasa1022 <tsubasa1022@gmail.com>
2019-08-11 21:01:08 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,14 +19,14 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
+  server '54.199.144.25', user: 'ec2-user', roles: %w{app db web}
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
